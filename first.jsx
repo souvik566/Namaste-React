9f9e2ac6057b4d18259416b1d7b97b0929dfd836
Souvik <souvikbhattacharyya417@gmail.com>
2024-04-06 01:41:03 +0530

    Namaste React has been started

---
+++ first.jsx
@@ -0,0 +1,10 @@
+let val=React.createElement(
+    "h1",
+    {
+        id:"Heading"
+    },
+    "Souvik Bhattacharyya"
+);
+ReactDOM.createRoot(document.getElementById("bdy")).render(
+    val
+);
